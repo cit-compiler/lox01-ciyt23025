42c2e3d7a8e3b27dba3ac55bce2c13273c2c9fb8
ciyt23025 <ciyt23025@g.nihon-u.ac.jp>
2025-11-25 11:56:06 +0900

    add Lox.java and TokenType.jave.

---
+++ codes/com/craftinginterpreters/lox/Lox.java
@@ -0,0 +1,31 @@
+package codes.com.craftinginterpreters.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Lox{
+    public static void main(String args[])throws IOException{
+        if(args.length > 1){
+            System.out.println("Usage jlox [script]");
+            System.exit(64);
+        }
+        else if(args.length == 1){
+            runFile(args[0]);
+        }
+        else{
+            runPrompt();
+        }
+    }
+    private static void runPrompt()throws IOException{
+        InputStreamReader input =new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+    }
+    private static void runFile(String path){
+        
+    }
+}
